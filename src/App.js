6158a67da059823c4603acdadf283b5d28114fdf
Rahul Kumar <94990093+RAHULKUMAR8383@users.noreply.github.com>
2024-08-25 22:48:28 +0530

    more added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 function App() {
   return (
    <div>
+      <h1 className="text-3xl text-red-500 font-bold underline"></h1>
+      <h1 className="text-3xl text-red-500 font-bold underline"></h1>
+      <h1 className="text-3xl text-red-500 font-bold underline"></h1>
+      <h1 className="text-3xl text-red-500 font-bold underline"></h1>
+      <h1 className="text-3xl text-red-500 font-bold underline"></h1>
       <h1 className="text-3xl text-red-500 font-bold underline">
       Hello world!
     </h1>
